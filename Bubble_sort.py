4c9056d4432269f81375b2c068126f4a87fd20b3
OrinaOisera <orinamark22@gmail.com>
2021-08-05 15:01:58 +0300

    Numbeers the pseudo code

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,15 +1,17 @@
 def findPair(A,sum):
-     for i in range(len(A)- 1):
+    x = 1
+    for i in range(len(A)- 1):
 
         for j in range(i+1, len(A)):
-             if A[i] + A[j] == sum:
-                 print("Pair forund is at index", i , "and", j)
+            if A[i] + A[j] == sum:
+                print(A[i], "&", A[j])
+                print("Pair found is at index", i, "and", j)
 
-     print("Pair found")
-
+        print("Pair found", x)
+        x += 1
 
 
 A = [5, 7, 4, 3, 9, 8, 19, 2]
 sum =12
 
-findPair(A,sum)
+findPair(A, sum)
